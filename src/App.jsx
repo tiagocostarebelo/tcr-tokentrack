fe05b97c3c2a44e3c939864e7edbc926ee872881
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2025-08-13 16:40:16 +0100

    set up api_url and useEffect hook to fecth data using .then first. will change for async await next

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
 import tokenTrackLogo from './assets/TokenTrack.png';
+import { useState, useEffect } from 'react';
+const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
 
 const App = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => {
+        if (!res.ok) throw new Error('Failed to fetch data');
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setCoins(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      })
+  }, [])
+
   return (
     <div className="header-logo">
       <img src={tokenTrackLogo} alt="logo" className="logo"></img>
