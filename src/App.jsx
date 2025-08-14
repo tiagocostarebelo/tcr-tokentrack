f957e3c8fe5267c36da5db39403d15160f245970
Tiago Costa Rebelo <43050965+tiagocostarebelo@users.noreply.github.com>
2025-08-14 10:18:36 +0000

    added new component CoinCard to render the coins in the UI. Added async await instead of .then

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import tokenTrackLogo from './assets/TokenTrack.png';
+import tokenTrackLogo from './assets/tokentrack.png';
 import { useState, useEffect } from 'react';
-const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+import CoinCard from './components/CoinCard';
+const API_URL = 'https://cors-anywhere.herokuapp.com/https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
 
 const App = () => {
   const [coins, setCoins] = useState([]);
@@ -8,26 +9,40 @@ const App = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(API_URL)
-      .then((res) => {
-        if (!res.ok) throw new Error('Failed to fetch data');
-        return res.json();
-      })
-      .then((data) => {
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(API_URL);
+        if(!res.ok) throw new Error('Failed to fetch data');
+        const data = await res.json();
         console.log(data);
         setCoins(data);
+      }
+      catch(err) {
+        setError(err.message);
+      }
+      finally{
         setLoading(false);
-      })
-      .catch((error) => {
-        setError(error.message);
-        setLoading(false);
-      })
+      }
+    }
+
+    fetchCoins();
   }, [])
 
   return (
-    <div className="header-logo">
-      <img src={tokenTrackLogo} alt="logo" className="logo"></img>
-      <h1>TokenTrack</h1>
+    <div>
+      <div className="header-logo">
+        <img src={tokenTrackLogo} alt="logo" className="logo"></img>
+        <h1>TokenTrack</h1>              
+      </div>
+      { loading && <p>Loading...</p>}
+      { error && <div className="error"> {error}</div> } 
+      { !loading && !error && (
+        <main className="grid">
+          {coins.map((coin) => (
+            <CoinCard coin={coin} key={coin.id}/>
+          ))}
+        </main>
+      )}
     </div>
   )
 }
