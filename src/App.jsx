d9cc352a74814ef4484271d6cad1eb643dbd0912
Tiago Costa Rebelo <43050965+tiagocostarebelo@users.noreply.github.com>
2025-08-14 13:01:29 +0000

    added filtering logic and input

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import CoinCard from './components/CoinCard';
 import LimitSelector from './components/LimitSelector';
 import CurrencySelector from './components/CurrencySelector';
+import FilterInput from './components/FilterInput';
 const API_URL = import.meta.env.VITE_API_URL;
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
   const [error, setError] = useState(null);
   const [currency, setCurrency] = useState("eur");
   const [limit, setLimit] = useState(10);
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
     const fetchCoins = async () => {
@@ -30,13 +32,20 @@ const App = () => {
     }
 
     fetchCoins();
-  }, [limit, currency])
+  }, [limit, currency]);
+
+  const filteredCoins = coins.filter((coin) => {
+    return coin.name.toLowerCase().includes(filter.toLowerCase()) || coin.symbol.toLowerCase().includes(filter.toLowerCase())
+  })
 
   return (
     <div>
       <div className="header-logo">
         <img src={tokenTrackLogo} alt="logo" className="logo"></img>
         <h1>TokenTrack</h1>              
+      </div>
+      <div className="top-controls">
+        <FilterInput filter={filter} onFilterChange={setFilter}/>        
       </div>
       <div className="controls">
         <CurrencySelector currency={currency} onCurrencyChange={setCurrency} />
@@ -46,9 +55,9 @@ const App = () => {
       { error && <div className="error"> {error}</div> } 
       { !loading && !error && (
         <main className="grid">
-          {coins.map((coin) => (
+          {filteredCoins.length > 0 ? filteredCoins.map((coin) => (
             <CoinCard coin={coin} currency={currency} key={coin.id}/>
-          ))}
+          )) : <p>No matching coins</p>}
         </main>
       )}
     </div>
