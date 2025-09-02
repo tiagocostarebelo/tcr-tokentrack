116e765893b34a899a06efcd93950c02a82c1577
Tiago Costa Rebelo <43050965+tiagocostarebelo@users.noreply.github.com>
2025-09-02 08:55:50 +0000

    fixed an error that was causing the app to fail

--- src/components/CoinCard.jsx
+++ src/components/CoinCard.jsx
@@ -12,7 +12,7 @@ const CoinCard = ({ coin, currency }) => {
           </div>
         </div>
         <p>Price: {currency === "eur" ? "€" : currency === "usd" ? "$" : "£"}{coin.current_price.toLocaleString()}</p>
-        <p className={coin.price_change_percentage_24h >= 0 ? 'positive' : 'negative'}>{coin.price_change_percentage_24h.toFixed(2)}%</p>
+        <p className={coin.price_change_percentage_24h >= 0 ? 'positive' : 'negative'}>{coin.price_change_percentage_24h?.toFixed(2)}%</p>
         <p>Market Cap: {coin.market_cap.toLocaleString()}</p>
       </div>
     </Link>
